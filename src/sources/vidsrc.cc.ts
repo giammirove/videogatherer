82c872350bfef9cf117f37a1f7ad2c64b436904a
Gianmaria Rovelli <rovelli.gianmaria@gmail.com>
2024-09-03 18:07:14 +0100

    fix: vidsrc.cc episode fixed

--- src/sources/vidsrc.cc.ts
+++ src/sources/vidsrc.cc.ts
@@ -19,6 +19,7 @@ type SourceJson = {
   },
   success: boolean
 }
+type ItemType = 'tv' | 'movie';
 
 const HOST = 'vidsrc.cc';
 const ALT_HOSTS = [HOST];
@@ -32,14 +33,15 @@ function enc(input: string): string {
   return encodeURIComponent(btoa(rc4("NGWOrkeQxUHlD4wIGp4l", input)));
 }
 
-async function episode(postfix: string, id: string, _server = SERVER_UPCLOUD): Promise<Stream> {
+async function episode(id: string, type: ItemType = 'tv', s: number = 1, e: number = 1, _server = SERVER_UPCLOUD): Promise<Stream> {
+  const postfix: string = type == 'tv' ? `tv/${id}/${s}/${e}` : `movie/${id}`;
   const url: string = `https://${HOST}/v2/embed/${postfix}`;
   debug(ID, url);
   const res = await (await fetch(url)).text();
   const new_data_id = (new RegExp(`data-id="(.*?)" data-number=`, 'g')).exec(res)![1];
   const v_value = (new RegExp(`var v = "(.*?)"`, 'g')).exec(res)![1];
   debug(ID, new_data_id);
-  const api_url = `https://${HOST}/api/episodes/${new_data_id}/servers?id=${id}&isMobile=false&v=${encodeURIComponent(v_value)}&vrf=${encodeURIComponent(enc(id))}`;
+  const api_url = `https://${HOST}/api/episodes/${new_data_id}/servers?id=${id}&season=${s}&episode=${e}&type=${type}&isMobile=false&v=${encodeURIComponent(v_value)}&vrf=${encodeURIComponent(enc(id))}`;
   debug(ID, api_url);
   const res2: ServersJson = await (await fetch(api_url)).json() as ServersJson;
   const server_hash: ServerJson | undefined = res2.data.find(v => v.name == _server);
@@ -57,17 +59,17 @@ async function episode(postfix: string, id: string, _server = SERVER_UPCLOUD): P
 
 async function movie(id: string, _server?: string): Promise<Stream> {
   debug(ID, id);
-  return episode(`movie/${id}`, id, _server);
+  return episode(id, 'movie', 1, 1, _server);
 }
 
 async function tv(id: string, s: number = 1, e: number = 1, _server?: string): Promise<Stream> {
   debug(ID, id);
-  return episode(`tv/${id}/${s}/${e}`, id, _server);
+  return episode(id, 'tv', s, e, _server);
 }
 
 async function test() {
   try {
-    const tests = [movie("385687"), tv('tt0944947', 1, 1)];
+    const tests = [movie("385687"), tv('tt0944947', 2, 3)];
     const results = await Promise.all(tests);
     for (const r of results) {
       if (!isJSON(r))
