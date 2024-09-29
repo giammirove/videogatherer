41a5ed8df1d41267f6b99c982c946b0a0b6ae367
Gianmaria Rovelli <rovelli.gianmaria@gmail.com>
2024-09-30 00:49:19 +0100

    fix: vidsrc.cc key changed

--- src/sources/vidsrc.cc.ts
+++ src/sources/vidsrc.cc.ts
@@ -24,33 +24,68 @@ type ItemType = 'tv' | 'movie';
 const HOST = 'vidsrc.cc';
 const ALT_HOSTS = [HOST];
 const SERVER_UPCLOUD: string = 'UpCloud';
+const SERVER_VIDCLOUD: string = 'VidCloud';
 const SERVERS: Array<ServerListItem> = [
   { id: SERVER_UPCLOUD, handler: UpCloud },
 ];
 const ID = "VSC";
+const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36';
 
 function enc(input: string): string {
-  return encodeURIComponent(btoa(rc4("NGWOrkeQxUHlD4wIGp4l", input)));
+  let key = "78B22E5E862BC";
+  return encodeURIComponent(btoa(btoa(rc4(key, input))));
 }
 
-async function episode(id: string, type: ItemType = 'tv', s: number = 1, e: number = 1, _server = SERVER_UPCLOUD): Promise<Stream> {
+async function episode(id: string, type: ItemType = 'tv', s: number = 1, e: number = 1, _server = SERVER_VIDCLOUD): Promise<Stream> {
   const postfix: string = type == 'tv' ? `tv/${id}/${s}/${e}` : `movie/${id}`;
   const url: string = `https://${HOST}/v2/embed/${postfix}`;
   debug(ID, url);
-  const res = await (await fetch(url)).text();
+  const res = await (await fetch(url, {
+    headers: {
+      'Host': HOST,
+      'User-Agent': USER_AGENT,
+      'Sec-Fetch-Site': 'same-origin',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Dest': 'iframe',
+      'Referer': HOST,
+    }
+  })).text();
   const new_data_id = (new RegExp(`data-id="(.*?)" data-number=`, 'g')).exec(res)![1];
-  const v_value = (new RegExp(`var v = "(.*?)"`, 'g')).exec(res)![1];
   debug(ID, new_data_id);
-  const api_url = `https://${HOST}/api/episodes/${new_data_id}/servers?id=${id}&season=${s}&episode=${e}&type=${type}&isMobile=false&v=${encodeURIComponent(v_value)}&vrf=${encodeURIComponent(enc(id))}`;
+  const v_value = (new RegExp(`var v = "(.*?)"`, 'g')).exec(res)![1];
+  debug(ID, `v value: ${v_value}`);
+  const vrf = encodeURIComponent(enc(id));
+  debug(ID, `vrf: ${vrf}`);
+  const api_url = `https://${HOST}/api/episodes/${new_data_id}/servers?id=${id}&season=${s}&episode=${e}&type=${type}&isMobile=false&v=${encodeURIComponent(v_value)}&vrf=${vrf}`;
   debug(ID, api_url);
-  const res2: ServersJson = await (await fetch(api_url)).json() as ServersJson;
+  const res2: ServersJson = await (await fetch(api_url, {
+    headers: {
+      'Host': HOST,
+      'User-Agent': USER_AGENT,
+      'Referer': HOST,
+    }
+  }
+  )).json() as ServersJson;
   const server_hash: ServerJson | undefined = res2.data.find(v => v.name == _server);
-  debug(ID, server_hash.hash);
+  debug(ID, `server hash: ${server_hash.hash}`);
   if (server_hash == undefined)
     throw NO_STREAM_ERROR;
-  const embed_url = `https://${HOST}/api/source/${server_hash.hash}?init=true`;
-  debug(ID, embed_url);
-  const res3: SourceJson = await (await fetch(embed_url)).json() as SourceJson;
+  // not used at the moment ... it could be useful in the future
+  const key = encodeURIComponent(btoa(id + "-" + new_data_id));
+  debug(ID, `key: ${key}`);
+  const embed_url = `https://${HOST}/api/source/${server_hash.hash}`;
+  debug(ID, `embed url: ${embed_url}`);
+  const res3: SourceJson = await (await
+    fetch(embed_url, {
+      headers: {
+        'Host': HOST,
+        'User-Agent': USER_AGENT,
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+      }
+    })
+  ).json() as SourceJson;
   if (res3.success == false)
     throw NO_STREAM_ERROR;
   debug(ID, res3.data.source)
@@ -69,7 +104,8 @@ async function tv(id: string, s: number = 1, e: number = 1, _server?: string): P
 
 async function test() {
   try {
-    const tests = [movie("385687"), tv('tt0944947', 2, 3)];
+    //const tests = [movie("385687"), tv('tt0944947', 2, 3)];
+    const tests = [tv('tt0944947', 2, 3)];
     const results = await Promise.all(tests);
     for (const r of results) {
       if (!isJSON(r))
